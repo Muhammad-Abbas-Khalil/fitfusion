9be352c2a40802588b71b983c85be4a14823224b
Muhammad Abbas Khalil <abbaskhalil.bcs@gmail.com>
2026-04-07 05:53:02 +0500

    Remove DS_Store, hide database credentials

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:11223344@localhost:5432/fitfusion_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://localhost/fitfusion_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
